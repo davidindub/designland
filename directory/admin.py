270e1093edc6112ac803a112f9ef3e3c550db500
David <davidpkelly@me.com>
2022-08-04 11:11:29 +0000

    DOCS: Add wireframe

--- directory/admin.py
+++ directory/admin.py
@@ -1,16 +1,22 @@
 from django.contrib import admin
 from .models import Resource, Comment
 
-
-# Auto creates the slug based on the title
 @admin.register(Resource)
 class ResourceAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
     list_filter = ("approved", "created_on")
     list_display = ("title", "author", "created_on", "approved")
+    actions = ["approve_resource"]
+
+    def approve_resource(self, request, queryset):
+        queryset.update(approved=True)
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_filter = ("approved", "created_on")
     list_display = ("resource", "body", "author", "created_on", "approved")
+    actions = ["approve_comments"]
+
+    def approve_comments(self, request, queryset):
+            queryset.update(approved=True)
